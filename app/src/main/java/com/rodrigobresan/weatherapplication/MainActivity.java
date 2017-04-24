1fb01d8efd3560014a97f48d9969e7479644244c
Rodrigo Bresan <rodrigo.bresan@cleevio.cz>
2017-04-24 18:25:53 +0200

    Add MainActivity into Manifest file

---
+++ app/src/main/java/com/rodrigobresan/weatherapplication/MainActivity.java
@@ -0,0 +1,21 @@
+package com.rodrigobresan.weatherapplication;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by rodrigobresan on 4/24/17.
+ * <p>
+ * E-mail: rcbresan@gmail.com
+ * Github: bresan
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
