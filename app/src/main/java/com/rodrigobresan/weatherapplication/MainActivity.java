5505204527a78727f53c8ec0053bd0b808f64c6c
Rodrigo Bresan <rodrigo.bresan@cleevio.cz>
2017-04-25 10:58:18 +0200

    Buggy code that should fail

--- app/src/main/java/com/rodrigobresan/weatherapplication/MainActivity.java
+++ app/src/main/java/com/rodrigobresan/weatherapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.rodrigobresan.weatherapplication;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 /**
  * Created by rodrigobresan on 4/24/17.
@@ -16,6 +17,17 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        someMethod(1);
+    }
+
+    private void someMethod(int variable) {
+        switch (variable) {
+            case 1:
+                System.out.println("1");
+            case 2:
+                System.out.println("2");
+        }
     }
 
 }
