d7fd6f86ac0af130259df04f44124d67ee9ab4f6
Rodrigo Bresan <rodrigo.bresan@cleevio.cz>
2017-04-25 12:10:07 +0200

    Minor change

--- app/src/main/java/com/rodrigobresan/weatherapplication/MainActivity.java
+++ app/src/main/java/com/rodrigobresan/weatherapplication/MainActivity.java
@@ -3,7 +3,6 @@ package com.rodrigobresan.weatherapplication;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
 /**
  * Created by rodrigobresan on 4/24/17.
