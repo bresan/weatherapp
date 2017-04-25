c6b5cd73712a810a4ca48160621613666d854a4a
Rodrigo Bresan <rodrigo.bresan@cleevio.cz>
2017-04-25 11:24:30 +0200

    Unused call

--- app/src/main/java/com/rodrigobresan/weatherapplication/MainActivity.java
+++ app/src/main/java/com/rodrigobresan/weatherapplication/MainActivity.java
@@ -17,17 +17,5 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        someMethod(1);
     }
-
-    private void someMethod(int variable) {
-        switch (variable) {
-            case 1:
-                System.out.println("1");
-            case 2:
-                System.out.println("2");
-        }
-    }
-
 }
