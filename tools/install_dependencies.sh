25b11109232959bf6ae5b55677219f0988b2003b
Rodrigo Bresan <rodrigo.bresan@cleevio.cz>
2017-04-26 13:18:25 +0200

    Fix path on install dependency

--- tools/install_dependencies.sh
+++ tools/install_dependencies.sh
@@ -6,7 +6,7 @@ export PATH="$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$PATH"
 DEPS="$ANDROID_HOME/installed-dependencies"
 
 if [ ! -e $DEPS ]; then
-  cp -r /usr/local/android-sdk-linux $ANDROID_HOME &&
+  cp -r /usr/local $ANDROID_HOME &&
   echo y | android update sdk -u -a -t build-tools-25.0.2 &&
   echo y | android update sdk -u -a -t android-25 &&
   echo y | android update sdk -u -a -t extra-android-m2repository &&
