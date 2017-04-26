e78beeb75103b615756ae5d5ed5097b4805a079f
Rodrigo Bresan <rodrigo.bresan@cleevio.cz>
2017-04-26 13:15:48 +0200

    Install dependencies script

---
+++ tools/install_dependencies.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Fix the CircleCI path
+export PATH="$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$PATH"
+
+DEPS="$ANDROID_HOME/installed-dependencies"
+
+if [ ! -e $DEPS ]; then
+  cp -r /usr/local/android-sdk-linux $ANDROID_HOME &&
+  echo y | android update sdk -u -a -t build-tools-25.0.2 &&
+  echo y | android update sdk -u -a -t android-25 &&
+  echo y | android update sdk -u -a -t extra-android-m2repository &&
+  echo y | android update sdk -u -a -t extra-google-m2repository &&
+  echo y | android update sdk -u -a -t extra-google-google_play_services &&
+  echo y | android update sdk -u -a -t extra-android-support &&
+  touch $DEPS
+fi
