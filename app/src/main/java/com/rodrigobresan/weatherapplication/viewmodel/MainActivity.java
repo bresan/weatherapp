6efad7cf36dff68e182dd11c6a14c8b4ffd1fe14
Rodrigo Bresan <rodrigo.bresan@cleevio.cz>
2017-04-26 10:37:10 +0200

    Circle.yml file added

--- app/src/main/java/com/rodrigobresan/weatherapplication/viewmodel/MainActivity.java
+++ app/src/main/java/com/rodrigobresan/weatherapplication/viewmodel/MainActivity.java
@@ -1,8 +1,12 @@
-package com.rodrigobresan.weatherapplication;
+package com.rodrigobresan.weatherapplication.viewmodel;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+
+import com.rodrigobresan.weatherapplication.R;
+import com.rodrigobresan.weatherapplication.databinding.ActivityMainBinding;
 
 /**
  * Created by rodrigobresan on 4/24/17.
@@ -13,8 +17,12 @@ import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding mBinding;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
     }
 }
