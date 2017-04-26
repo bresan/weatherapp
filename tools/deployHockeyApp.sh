32540e69e25893953f3ebfabf72380aa3aa42fd6
Rodrigo Bresan <rodrigo.bresan@cleevio.cz>
2017-04-26 19:09:23 +0200

    Add test coverage with Jacoco

--- tools/deployHockeyApp.sh
+++ tools/deployHockeyApp.sh
@@ -28,7 +28,30 @@
 # release_type=2    # beta
 # X-HockeyAppToken: token of app owner
 
-function uploadToHockeyApp {
+function uploadToHockeyAppDevelopment {
+
+  GIT_COMPARE_KEY=${CIRCLE_COMPARE_URL##*/}
+  GIT_PRETTY_COMMIT_LOG=$(echo "<ul>$(git log ${GIT_COMPARE_KEY} --pretty=format:'<li>[%ad] %s (%an)</li>' --date=short)</ul>" | tr -d '\n')
+
+  HOCKEYAPP_NOTES_HEADER="**Built on:** $(date +"%a %d-%b-%Y %I:%M %p")
+  **Branch:** $(git rev-parse --abbrev-ref HEAD)
+  **Commit:** $(git rev-parse --short HEAD)"
+
+  HOCKEYAPP_NOTES_HEADER_HTML=${HOCKEYAPP_NOTES_HEADER//$'\n'/<br>}
+  HOCKEYAPP_NOTES="${HOCKEYAPP_NOTES_HEADER_HTML} ${GIT_PRETTY_COMMIT_LOG}"
+
+  curl --verbose \
+       --fail \
+       --form "status=2" \
+       --form "notify=1" \
+       --form "notes=${HOCKEYAPP_NOTES}" \
+       --form "notes_type=0" \
+       --form "ipa=@app/build/outputs/apk/app-free-debug.apk" \
+       --header "X-HockeyAppToken: ${HOCKEYAPP_TOKEN}" \
+       "https://rink.hockeyapp.net/api/2/apps/${HOCKEYAPP_APP_ID}/app_versions/upload"
+}
+
+function uploadToHockeyAppRelease {
 
   GIT_COMPARE_KEY=${CIRCLE_COMPARE_URL##*/}
   GIT_PRETTY_COMMIT_LOG=$(echo "<ul>$(git log ${GIT_COMPARE_KEY} --pretty=format:'<li>[%ad] %s (%an)</li>' --date=short)</ul>" | tr -d '\n')
